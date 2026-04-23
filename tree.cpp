36e88974edceacb52405ce2c9af19da1cfe1a941
Akshit Agrawal <akshitisagrawal@gmail.com>
2026-04-23 21:04:11 +0530

    a

--- tree.cpp
+++ tree.cpp
@@ -2,10 +2,38 @@
 using namespace std;
 
 struct node{
-    int data
-    node* left=nullptr
-    node* right=nullptr
+    int data;
+    node* left=nullptr;
+    node* right=nullptr;
     node(int value){
-        this.val=value
+        data=value;
     }
+};
+
+void inorder(node* root){
+    if (root!=nullptr){
+        inorder(root->left);
+        cout<<(root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(node* root){
+    if (root!=nullptr){
+        cout<<(root->data);
+        inorder(root->left);
+        inorder(root->right);
+    }
+}
+
+void postorder(node* root){
+    if (root!=nullptr){
+        inorder(root->left);
+        inorder(root->right);
+        cout<<(root->data);
+    }
+}
+
+int main(){
+   
 }
