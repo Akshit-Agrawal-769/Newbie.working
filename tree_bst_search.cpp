6b64367142fbd7fba7624f9e8d9d8446abae7cd7
Akshit Agrawal <akshitisagrawal@gmail.com>
2026-04-23 22:59:15 +0530

    a

--- tree_bst_search.cpp
+++ tree_bst_search.cpp
@@ -14,7 +14,7 @@ void inorder(node* root){
     if (root!=nullptr){
         inorder(root->left);
         cout<<root->data<<' ';
-        inorder(root->left);
+        inorder(root->right);
     }
 }
 
@@ -35,5 +35,27 @@ node* search(node* root, int val){
 }
 
 int main(){
-    
+    node* root = new node(4);
+    node* a1 = new node(2);
+    node* a2 = new node(5);
+    node* b1 = new node(1);
+    node* b2 = new node(3);
+
+    root->left=a1;
+    root->right=a2;
+    a1->left=b1;
+    a1->right=b2;
+
+    inorder(root);
+
+    node* find = search(root,5);
+    if (find==nullptr){
+        cout<<"f off";
+    }
+    else{
+        cout<<"yay";
+    }
+
+    system("pause");
+    return 0;
 }
