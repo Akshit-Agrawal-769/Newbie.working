900952bff5c3ea164ebff0d06fa309a5df472bc8
Akshit Agrawal <akshitisagrawal@gmail.com>
2026-04-23 22:14:11 +0530

    populate yea

--- tree.cpp
+++ tree.cpp
@@ -50,27 +50,27 @@ node* populate(){
 
 
 int main(){
-    node* root= new node(4);
-    node* a1= new node(2);
-    node* a2= new node(6);
-    node* l1= new node(1);
-    node* l2= new node(3);
-    node* l3= new node(5);
-    node* l4= new node(7);
+    // node* root= new node(4);
+    // node* a1= new node(2);
+    // node* a2= new node(6);
+    // node* l1= new node(1);
+    // node* l2= new node(3);
+    // node* l3= new node(5);
+    // node* l4= new node(7);
 
-    root->left=a1;
-    root->right=a2;
-    a1->left=l1;
-    a1->right=l2;
-    a2->left=l3;
-    a2->right=l4;
+    // root->left=a1;
+    // root->right=a2;
+    // a1->left=l1;
+    // a1->right=l2;
+    // a2->left=l3;
+    // a2->right=l4;
 
+    cout<<"Enter root: ";
+    node* root=populate();
+    cout<<"\nInorder: ";
 
     inorder(root);
     cout<<endl;
-    preorder(root);
-    cout<<endl;
-    postorder(root);
 
     system("pause");
     return 0;
