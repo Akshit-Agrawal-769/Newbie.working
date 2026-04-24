8e1ab68a842c387924a61949652ad4a7bbaf9cb9
Akshit Agrawal <akshitisagrawal@gmail.com>
2026-04-24 12:13:30 +0530

    a

--- tree_bstContructor.cpp
+++ tree_bstContructor.cpp
@@ -1,8 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+struct Node{
     Node* left=nullptr;
     Node* right=nullptr;
     int data;
@@ -11,26 +10,41 @@ struct Node
     }
 };
 
-Node* insert(Node* node,int val){
-    if (node==nullptr){
-        node = new Node(val);
-        return node;
-    }
-    if (val< node->data){
-        node->left=insert(node->left, val);
-    }
-     if (val> node->data){
-        node->right=insert(node->right, val);
-    } 
-    return node;
-}  
+Node* insert(Node* root, int val){
+    if(root==nullptr) return new Node(val);
+    if(val < root->data)
+        root->left=insert(root->left, val);
+    if(val > root->data)
+        root->right=insert(root->right, val);
+    return root;
+}
 
-Node* bst_constructor(int sortedArr,int start, int end){
+Node* bst_constructor(int arr[], int start, int end){
+    if(start > end) return nullptr;      
+
     int mid=(start+end)/2;
+    Node* root=new Node(arr[mid]);
+
+    root->left=bst_constructor(arr, start, mid-1);
+    root->right=bst_constructor(arr, mid+1, end);
+
+    return root;
+}
+
+void inorder(Node* root){
+    if(root!=nullptr){
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
 }
 
 int main(){
+    int arr[]={1,2,3,4,5,6,7};
+    int n=sizeof(arr)/sizeof(arr[0]);
 
+    Node* root=bst_constructor(arr, 0, n-1);
+    inorder(root); // prints: 1 2 3 4 5 6 7
 
     system("pause");
     return 0;
