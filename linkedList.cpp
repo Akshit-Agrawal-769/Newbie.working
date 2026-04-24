3dfb575cbd97bcfc050450ace0a327e52e44ca86
Akshit Agrawal <akshitisagrawal@gmail.com>
2026-04-24 17:42:57 +0530

    ll

--- linkedList.cpp
+++ linkedList.cpp
@@ -4,6 +4,9 @@ using namespace std;
 struct Node{
     int data;
     Node* next=nullptr;
+    Node(int val){
+        data=val;
+    }
 };
 
 void traverse(Node* node){
@@ -14,11 +17,41 @@ void traverse(Node* node){
     }
 }
 
-void insert(){
-
+void insertHead(Node* &head, int val){
+    Node* newNode=new Node(val);
+    newNode->next=head;
+    head=newNode;
 }
 
-void delete(){
+void insertEnd(Node* &head, int val){
+    Node* newNode=new Node(val);
+    if(head==nullptr){
+        head=newNode;
+        return;
+    }
+    Node* cursor=head;
+    while(cursor->next!=nullptr){
+        cursor=cursor->next;
+    }
+    cursor->next=newNode;
+}
+
+void insertPos(Node* &head, int val, int pos){
+    if(pos==0){
+        insertHead(head,val);
+        return;
+    }
+    Node* newNode=new Node(val);
+    Node* cursor=head;
+    for(int i=0;i<pos-1;i++){
+        if(cursor==nullptr){
+            cout<<"Position out of range\n";
+            return;
+        }
+        cursor=cursor->next;
+    }
+    newNode->next=cursor->next;
+    cursor->next=newNode;
 
 }
 
